2528d7c07481188ec830817480caf9358304d677
DonCAI <aidonc1@centrum.cz>
2025-06-27 23:01:17 +0200

    Update: [kontaktni formular validation error fix]

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify"; // Import the Netlify adapter
 
 export default defineConfig({
   integrations: [tailwind()],
   output: 'hybrid',
+  adapter: netlify(), // Add the adapter here
 });
